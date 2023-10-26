8b6459556ae83a69a38a49543f297a112b5cdadc
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2023-10-26 09:28:37 -0600

    Add project files.

---
+++ Open_The_Safe/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Open_The_Safe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            numericUpDown3 = new NumericUpDown();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Enabled = false;
+            numericUpDown1.Location = new Point(277, 146);
+            numericUpDown1.Maximum = new decimal(new int[] { 9, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(52, 31);
+            numericUpDown1.TabIndex = 0;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Enabled = false;
+            numericUpDown2.Location = new Point(335, 146);
+            numericUpDown2.Maximum = new decimal(new int[] { 9, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(51, 31);
+            numericUpDown2.TabIndex = 1;
+            numericUpDown2.ValueChanged += numericUpDown2_ValueChanged;
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Enabled = false;
+            numericUpDown3.Location = new Point(392, 146);
+            numericUpDown3.Maximum = new decimal(new int[] { 9, 0, 0, 0 });
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(52, 31);
+            numericUpDown3.TabIndex = 2;
+            numericUpDown3.ValueChanged += numericUpDown3_ValueChanged;
+            // 
+            // button1
+            // 
+            button1.Enabled = false;
+            button1.Location = new Point(450, 146);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 3;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Enabled = false;
+            textBox1.Location = new Point(344, 183);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 31);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(763, 319);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 4;
+            button2.Text = "Cheat";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Enabled = false;
+            textBox2.Location = new Point(648, 35);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(179, 230);
+            textBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(52, 55);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 6;
+            button3.Text = "Start";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(170, 55);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 34);
+            button4.TabIndex = 7;
+            button4.Text = "Exit";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Enabled = false;
+            pictureBox1.Location = new Point(26, 210);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(256, 143);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(887, 365);
+            Controls.Add(pictureBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(textBox2);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(numericUpDown3);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown3;
+        private Button button1; //submit
+        private TextBox textBox1; //cheat log
+        private Button button2; //cheat
+        private TextBox textBox2; // log
+        private Button button3; //start
+        private Button button4; //exit
+        private PictureBox pictureBox1;
+        private int[] code;
+        Random random;
+        private int lives;
+    }
+}
---
+++ Open_The_Safe/Form1.cs
@@ -0,0 +1,99 @@
+namespace Open_The_Safe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            code = new int[3];
+            random = new Random();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Submit button
+            checkWin();
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Start Button Click
+            for (int i = 0; i < 3; i++)
+            {
+                code[i] = random.Next(10);
+            }
+            lives = 3;
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+            numericUpDown3.Enabled = true;
+            button1.Enabled = true;
+            button3.Enabled = false;
+            textBox2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Cheat Button
+            string answer;
+            answer = code[0] + "," + code[1] + "," + code[2];
+            textBox1.Text = answer;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void checkWin()
+        {
+            if (numericUpDown1.Value == code[0] && numericUpDown2.Value == code[1] && numericUpDown3.Value == code[2])
+            {
+                numericUpDown2.Enabled = false;
+                numericUpDown1.Enabled = false;
+                numericUpDown3.Enabled = false;
+                button1.Enabled = false;
+                button3.Enabled = true;
+                textBox1.Text = "You Win";
+            } else
+            {
+                string turn;
+                turn = numericUpDown1.Value + "," + numericUpDown2.Value + "," + numericUpDown3.Value;
+                lives--;
+                if (lives <= 0)
+                {
+                    numericUpDown2.Enabled = false;
+                    numericUpDown1.Enabled = false;
+                    numericUpDown3.Enabled = false;
+                    button1.Enabled = false;
+                    button3.Enabled = true;
+                    textBox1.Text = "You LOSE!";
+                }
+                if ((numericUpDown1.Value == code[0] && numericUpDown2.Value == code[1]) || (numericUpDown1.Value == code[0] && numericUpDown3.Value == code[2]) || (numericUpDown3.Value == code[2] && numericUpDown2.Value == code[1]))
+                {
+                    turn += " - Close!";
+                }
+                else
+                {
+                    turn += " - way off you loser!";
+                }
+                textBox2.AppendText("\r\n" + turn);
+            }
+        }
+    }
+}
